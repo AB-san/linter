271c264dd5b4628f259e58d6a5477e6bc95270eb
Ansuman Behera <71064674+AB-san@users.noreply.github.com>
2021-04-19 14:11:38 +0530

    Update prog1.js

--- javascript/prog1.js
+++ javascript/prog1.js
@@ -1,11 +1,11 @@
 //  JavaScript program to swap two variables
 
 //  take input from the users
-let a = prompt('Enter the first variable: ');
-let b = prompt('Enter the second variable: ');
+let a = 10
+let b = 5
 
 //  create a temporary variable
-const temp;
+let temp = 0
 
 //  swap variables
 temp = a
